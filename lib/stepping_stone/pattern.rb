77fc56845ee5a89af38a98b79c45a785b16bdd02
Mike Sassak <msassak@gmail.com>
2011-03-03 09:26:23 -0600

    Matching against a class

--- lib/stepping_stone/pattern.rb
+++ lib/stepping_stone/pattern.rb
@@ -1,15 +1,21 @@
 module SteppingStone
   class Pattern
+    attr_reader :pattern
+
     def initialize(pattern)
       @pattern = pattern
     end
 
     def match(target)
-      self.===(target) 
+      self.===(target)
     end
 
     def ===(target)
-      @pattern === target
+      pattern === target
+    end
+
+    def to_s
+      "#{self.class}: '#{pattern}'"
     end
   end
 end
--- spec/stepping_stone/pattern_spec.rb
+++ spec/stepping_stone/pattern_spec.rb
@@ -1,18 +1,29 @@
 require 'spec_helper'
 
 module SteppingStone
-  describe Pattern, 'matching "blargle"' do
-    subject { Pattern.new("blargle") }
+  describe Pattern do
+    context 'matching "blargle"' do
+      subject { Pattern.new("blargle") }
 
-    it { should match("blargle") }
-    it { should_not match("fooble") }
-  end
+      it { should match("blargle") }
+      it { should_not match("fooble") }
+    end
 
-  describe Pattern, 'matching "/argle$/" against' do
-    subject { Pattern.new(/argle$/) }
-    
-    it { should match("blargle") }
-    it { should match("flargle") }
-    it { should_not match("fooble") }
+    context 'matching "/argle$/"' do
+      subject { Pattern.new(/argle$/) }
+      
+      it { should match("blargle") }
+      it { should match("flargle") }
+      it { should_not match("fooble") }
+    end
+
+    context 'matching String instances' do
+      subject { Pattern.new(String) } 
+
+      it { should match("blargle") }
+      it { should match("") }
+      it { should_not match(:blargle) }
+      it { should_not match(["fooble"]) }
+    end
   end
 end
