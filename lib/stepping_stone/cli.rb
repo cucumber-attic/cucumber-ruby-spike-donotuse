7b4a9b9c1f27f93c1dd27e99ce0a10e8ff5df4f8
Mike Sassak <msassak@gmail.com>
2011-05-19 23:22:26 -0500

    Update gems to their newest versions

--- lib/stepping_stone/cli.rb
+++ lib/stepping_stone/cli.rb
@@ -3,15 +3,24 @@ require 'kerplutz'
 module SteppingStone
   module Cli
     def self.go!(args)
-      # add sst/sst_helper to load path
-      # require every file in sst/lib
-      # Create a parser
-      # parser feature file, get test_case out
-      # server = RbServer.new
-      # formatter = ProgressFormatter.new
-      # test_case = TestCase.new(server, formatter)
-      # test_case.execute!
-      results = Kerplutz.build(:sst) do |base|
+      case parse(args)[0]
+      when :sst
+        puts "sst"
+      when :exec
+        puts "sst exec"
+        # add sst/sst_helper to load path
+        # require every file in sst/lib
+        # Create a parser
+        # parser feature file, get test_case out
+        # server = RbServer.new
+        # formatter = ProgressFormatter.new
+        # test_case = TestCase.new(server, formatter)
+        # test_case.execute!
+      end
+    end
+
+    def self.parse(args)
+      Kerplutz.build(:sst) do |base|
         base.banner = "Usage: #{base.name} [OPTIONS] COMMAND [ARGS]"
         base.action :version, "Show the version", abbrev: :V do
           puts "Stepping Stone v0.0.0"
@@ -20,13 +29,6 @@ module SteppingStone
         base.command :exec, "Execute a specification" do |cmd|
         end
       end.parse(args)
-
-      case results[0]
-      when "sst"
-        puts "sst"
-      when "exec"
-        puts "sst exec"
-      end
     end
   end
 end
