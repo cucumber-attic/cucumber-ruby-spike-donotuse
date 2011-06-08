ab070bbd844dbca404213c2106fc977f6317c629
Mike Sassak <msassak@gmail.com>
2011-06-07 23:36:42 -0500

    Remove unused code from TestCase

--- lib/stepping_stone/test_case.rb
+++ lib/stepping_stone/test_case.rb
@@ -2,52 +2,18 @@ module SteppingStone
   class TestCase
     include Enumerable
 
-    attr_reader :actions #, :result
+    attr_reader :actions
 
     def initialize(*actions)
       @actions = actions
-      #@result = :pending
     end
-
-    # Remove commented
-    #def execute!(server)
-      #with_start_and_end(server) do
-        #actions.each do |action|
-          #@result = server.apply(action)
-          #break unless passed?
-        #end
-      #end
-    #end
 
     def each(&blk)
       @actions.each(&blk)
     end
 
-    #def passed?
-      #result == :passed
-    #end
-
-    #def failed?
-      #result == :failed
-    #end
-
-    #def pending?
-      #result == :pending
-    #end
-
     def empty?
       @actions.empty?
     end
-
-    # Remove commented
-    #private
-
-    #def with_start_and_end(server, &block)
-      #if !actions.empty?
-        #server.start_test(self)
-        #block.call
-        #server.end_test(self)
-      #end
-    #end
   end
 end
