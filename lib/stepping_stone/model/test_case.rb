7639731f07c2efa7efd59cd9316482c5bd4dab82
Mike Sassak <msassak@gmail.com>
2011-06-16 21:11:28 -0500

    Note on proper identification of TestCase in the future

--- lib/stepping_stone/model/test_case.rb
+++ lib/stepping_stone/model/test_case.rb
@@ -5,6 +5,11 @@ module SteppingStone
 
       attr_reader :actions
 
+      # uri: TestCase identifier. URI fragments are allowed.
+      # actions: list of actions to be applied to the SUT for this test case
+      #
+      # def initialize(uri, actions)
+      #   @uri = uri
       def initialize(*actions)
         @actions = actions
       end
