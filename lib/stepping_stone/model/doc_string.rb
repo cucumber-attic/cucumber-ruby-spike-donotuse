5131bf82c5cac5cd78e6359f9a4742f33ad36a9a
Mike Sassak <msassak@gmail.com>
2011-06-20 19:06:51 -0500

    For the time being DocString is simply a subclass of String

--- lib/stepping_stone/model/doc_string.rb
+++ lib/stepping_stone/model/doc_string.rb
@@ -16,65 +16,7 @@ module SteppingStone
     #
     # Note how the indentation from the source is stripped away.
     #
-    class DocString #:nodoc:
-      class Builder
-        attr_reader :string
-
-        def initialize
-          @string = ''
-        end
-
-        def doc_string(string, line_number)
-          @string = string
-        end
-
-        def eof
-        end
-      end
-
-      attr_accessor :file
-
-      def self.default_arg_name
-        "string"
-      end
-
-      def self.parse(text)
-        builder = Builder.new
-        lexer = Gherkin::I18nLexer.new(builder)
-        lexer.scan(text)
-        new(builder.string)
-      end
-
-      def initialize(string)
-        @string = string
-      end
-
-      def to_step_definition_arg
-        @string
-      end
-
-      def accept(visitor)
-        return if Cucumber.wants_to_quit
-        visitor.visit_doc_string(@string)
-      end
-      
-      def arguments_replaced(arguments) #:nodoc:
-        string = @string
-        arguments.each do |name, value|
-          value ||= ''
-          string = string.gsub(name, value)
-        end
-        DocString.new(string)
-      end
-
-      def has_text?(text)
-        @string.index(text)
-      end
-
-      # For testing only
-      def to_sexp #:nodoc:
-        [:doc_string, to_step_definition_arg]
-      end
-    end
+    # DocString is a subclass of String in case we need to modify its behavior in the future.
+    DocString = Class.new(String)
   end
 end
