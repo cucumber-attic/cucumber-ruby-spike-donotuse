20aa7b7e7031a87340d8cf9b5465f00a1d062fc9
Mike Sassak <msassak@gmail.com>
2011-07-07 01:07:34 -0500

    Begin extraction of Executor class

--- features/step_definitions/cucumber_steps.rb
+++ features/step_definitions/cucumber_steps.rb
@@ -1,43 +1,69 @@
 Given /^a passing scenario "(.+)" with:$/ do |name, body|
   @test_case = SteppingStone::GherkinCompiler.new.compile("Feature: test\nScenario: #{name}\n" << body)[0]
-  # @test_sut = TestSut.new
-  # @test_sut.add_mapping
-  # @test_sut.add_mapper(calculator_mod)
-  @namespace = SteppingStone::TextMapper::Namespace.new
-  @namespace.add_mapping(SteppingStone::TextMapper::Mapping.new("I add 4 and 5", :add))
-  @namespace.add_mapping(SteppingStone::TextMapper::Mapping.new("the result is 9", :assert_result))
-  calculator_mod = Module.new do
-    include RSpec::Matchers # should be a way to add this module into context automagically
 
-    def add
-      @result = 4 + 5
+  class FakeSut
+    attr_reader :mappings, :hooks, :helpers
+
+    def initialize
+      @mappings = []
+      @hooks = []
+      @helpers = Module.new do
+        def add
+          @result = 5 + 4
+        end
+
+        def assert_result
+          @result.should eq(9)
+        end
+      end
     end
 
-    def assert_result
-      @result.should eq(9)
+    def add_mapping(from, to)
+      mappings << SteppingStone::TextMapper::Mapping.new(from, to)
+    end
+
+    def add_hook(signature, &blk)
+      hooks << SteppingStone::TextMapper::Hook.new(signature, &blk)
+    end
+
+    def find_mapping(from)
+      mappings.find { |mapping| mapping.match(from) } or raise(SteppingStone::TextMapper::Namespace::UndefinedMappingError.new(from))
+    end
+
+    def find_hook(from)
+      hooks.find { |hook| hook.match(from) }
+    end
+
+    def start_test(test_case)
+      @context = SteppingStone::TextMapper::Context.new(self, [RSpec::Matchers, helpers])
+      @context.setup(test_case)
+    end
+
+    def end_test(test_case)
+      @context.teardown(test_case)
+    end
+
+    def apply(action)
+      @context.dispatch(action)
     end
   end
-  @namespace.add_mapper(calculator_mod)
+
+  @sut = FakeSut.new
+  @sut.add_mapping("I add 4 and 5", :add)
+  @sut.add_mapping("the result is 9", :assert_result)
 end
 
 Given /^these passing hooks:$/ do |hooks|
   hooks.rows.each do |aspect, subject|
     hook_signature = [aspect.to_sym, subject.tr(' ', '_').to_sym]
-    @namespace.add_hook(SteppingStone::TextMapper::Hook.new(hook_signature) { |test_case| test_case.name })
+    @sut.add_hook(hook_signature) { |test_case| test_case.name }
   end
 end
 
 When /^Cucumber executes the scenario "(.+)"$/ do |name|
-  # executor = SteppingStone::Model::Executor.new(sut)
-  # executor.execute(test_case)
-  context = @namespace.build_context
-  @events = []
-  @events << [:before, context.setup(@test_case)]
-  @test_case.inject(@events) do |events, action|
-    context.dispatch(action)
-    events << [:dispatch, action[0]]
-  end
-  @events << [:after, context.teardown(@test_case)]
+  executor = SteppingStone::Model::Executor.new(@sut)
+  executor.execute(@test_case)
+  @events = executor.events
 end
 
 Then /^the life cycle events are:$/ do |table|
---
+++ lib/stepping_stone/model/executor.rb
@@ -0,0 +1,27 @@
+module SteppingStone
+  module Model
+    class Executor
+      attr_reader :sut, :events
+
+      def initialize(sut)
+        @sut = sut
+        @events = []
+      end
+
+      def execute(test_case)
+        event(:before, sut.start_test(test_case))
+        test_case.each do |action|
+          sut.apply(action)
+          event(:dispatch, action[0])
+        end
+        event(:after, sut.end_test(test_case))
+      end
+
+      private
+
+      def event(name, value)
+        @events << [name, value]
+      end
+    end
+  end
+end
