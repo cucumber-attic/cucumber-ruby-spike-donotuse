a6599dff8af6f913a0c6cef1aff62ecc351e31a5
Mike Sassak <msassak@gmail.com>
2011-07-15 23:31:04 -0500

    Remove FakeSut entirely

--- features/step_definitions/cucumber_steps.rb
+++ features/step_definitions/cucumber_steps.rb
@@ -1,62 +1,33 @@
 Given /^a passing scenario "(.+)" with:$/ do |name, body|
   @test_case = SteppingStone::GherkinCompiler.new.compile("Feature: test\nScenario: #{name}\n" << body)[0]
+  @sut = SteppingStone::RbServer.new
 
-  class FakeSut
-    attr_reader :mappings, :hooks, :helpers
-
-    def initialize
-      @mappings = SteppingStone::TextMapper::MappingPool.new
-      @hooks = []
-      @helpers = Module.new do
-        def add
-          @result = 5 + 4
-        end
-
-        def assert_result
-          @result.should eq(9)
-        end
-      end
-    end
-
-    def add_mapping(from, to)
-      mappings.add(SteppingStone::TextMapper::Mapping.new(from, to))
-    end
-
-    def add_hook(signature, &blk)
-      hooks << SteppingStone::TextMapper::Hook.new(signature, &blk)
-    end
-
-    def find_mapping(from)
-      mappings.find!(from)
-    end
-
-    def find_hook(from)
-      hooks.find { |hook| hook.match(from) }
-    end
-
-    def start_test(test_case)
-      @context = SteppingStone::TextMapper::Context.new(self, [RSpec::Matchers, helpers])
-      SteppingStone::Model::Event.new(:before, :event, @context.setup(test_case))
-    end
-
-    def end_test(test_case)
-      SteppingStone::Model::Event.new(:after, :event, @context.teardown(test_case))
-    end
-
-    def dispatch(action)
-      SteppingStone::Model::Event.new(action, :passed, @context.dispatch(action))
-    end
+  unless defined?(Dsl)
+    Dsl = @sut.dsl_module
   end
 
-  @sut = FakeSut.new
-  @sut.add_mapping(["I add 4 and 5"], :add)
-  @sut.add_mapping(["the result is 9"], :assert_result)
+  module TestMapper
+    extend Dsl
+    include RSpec::Matchers
+
+    def_map "I add 4 and 5" => :add
+    def_map "the result is 9" => :assert_result
+
+    def add
+      @result = 5 + 4
+    end
+
+    def assert_result
+      @result.should eq(9)
+    end
+  end
 end
 
 Given /^these passing hooks:$/ do |hooks|
   hooks.rows.each do |aspect, subject|
     hook_signature = [aspect.to_sym, subject.tr(' ', '_').to_sym]
-    @sut.add_hook(hook_signature) { |test_case| test_case.name }
+    hook = SteppingStone::TextMapper::Hook.new(hook_signature) { |test_case| test_case.name }
+    @sut.mapper_namespace.add_hook(hook)
   end
 end
 
