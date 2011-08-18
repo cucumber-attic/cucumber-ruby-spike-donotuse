1319236b0bfc0c50c52d4e4211a00ccf08c41c46
Mike Sassak <msassak@gmail.com>
2011-08-18 00:20:35 -0500

    Context#dispatch calls the mapping when it exists

--- spec/stepping_stone/servers/rb/context_spec.rb
+++ spec/stepping_stone/servers/rb/context_spec.rb
@@ -4,6 +4,8 @@ module SteppingStone
   module Servers
     module Rb
       describe "Context" do
+        subject { Context.new }
+
         describe "#mappers=" do
           it "mixes in the methods from the helper modules" do
             helper_a = Module.new do
@@ -18,10 +20,23 @@ module SteppingStone
               end
             end
 
-            context = Context.new
-            context.mappers = [helper_a, helper_b]
-            context.helper_a.should eq(:helper_a)
-            context.helper_b.should eq(:helper_b)
+            subject.mappers = [helper_a, helper_b]
+            subject.helper_a.should eq(:helper_a)
+            subject.helper_b.should eq(:helper_b)
+          end
+        end
+
+        describe "#dispatch" do
+          context "when a mapping exists" do
+            it "invokes the mapping" do
+              mappings = double("mappings")
+              mapping  = double("mapping")
+              mappings.stub(:find_mapping).and_return(mapping)
+              subject.mappings = mappings
+
+              mapping.should_receive(:call).with(subject, [:foo])
+              subject.dispatch([:foo])
+            end
           end
         end
       end
